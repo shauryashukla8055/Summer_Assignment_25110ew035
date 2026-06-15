199e8df111aa03c53a5b4d31759a6763dbd09573
shauryashukla8055 <shauryashukla8055@gmail.com>
2026-06-15 10:21:23 +0530

    Add files via upload
    day 2 summer assignment

---
+++ day_2/program_2.c
@@ -0,0 +1,15 @@
+program_3.c
+#include<stdio.h>
+int main()
+{
+    int n,rev=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rev=rev*10+n%10;
+        n=n/10;
+    }
+    printf("Reverse of the number is %d",rev);
+    return 0;
+}
---
+++ day_2/program_3.c
@@ -0,0 +1,15 @@
+program_3#include<stdio.h>
+int main()
+{
+    int n,digit,product=1;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        digit=n%10;
+        product=product*digit;
+        n=n/10;
+    }
+    printf("Product of digits is %d",product);
+    return 0;
+}
